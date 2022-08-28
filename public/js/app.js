15ec6e9e951854fdd11e1664ef3f789914dcfe66
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2022-08-28 13:58:48 +0300

    Scenes added and app flow coded
    Added:
    - Scenes (videos, views) - as per the scenario (public/images/scene_flow.jpg)
    - Navigation between scenes
    - "Dev" menu
    
    Plus...
    - some styles redefined
    - commands added to package.json

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,16 @@
 (function () {
   function init() {
     new Router([
-      new Route("home", "home.html", true),
-      new Route("video1", "video1.html"),
-      new Route("video2", "video2.html"),
-      new Route("video3", "video3.html"),
+      new Route("start", "start.html"),
+      new Route("sceneA", "sceneA.html"),
+      new Route("sceneB", "sceneB.html"),
+      new Route("sceneC1", "sceneC1.html"),
+      new Route("sceneC2", "sceneC2.html"),
+      new Route("sceneD", "sceneD.html"),
+      new Route("sceneE", "sceneE.html"),
+      new Route("sceneF", "sceneF.html"),
+      new Route("end", "end.html"),
+      new Route("dev", "dev.html", true),
     ]);
   }
   init();
