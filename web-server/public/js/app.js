72d3e46b9609e6013d615a5236cf533cc1707fa1
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2022-08-29 10:46:39 +0300

    Scenes connectors added
    Views, videos, subtitles for connectors added.
    Scene flow updated.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -3,6 +3,7 @@
 (function () {
   function init() {
     new Router([
+      new Route("dev", "dev.html", true),
       new Route("start", "start.html"),
       new Route("sceneA", "sceneA.html"),
       new Route("sceneB", "sceneB.html"),
@@ -12,7 +13,15 @@
       new Route("sceneE", "sceneE.html"),
       new Route("sceneF", "sceneF.html"),
       new Route("end", "end.html"),
-      new Route("dev", "dev.html", true),
+      new Route("connectAB", "connectAB.html"),
+      new Route("connectAF", "connectAF.html"),
+      new Route("connectBC", "connectBC.html"),
+      new Route("connectBD", "connectBD.html"),
+      new Route("connectCE", "connectCE.html"),
+      new Route("connectDC", "connectDC.html"),
+      new Route("connectDE", "connectDE.html"),
+      new Route("connectFC", "connectFC.html"),
+      new Route("connectFE", "connectFE.html"),
     ]);
   }
   init();
