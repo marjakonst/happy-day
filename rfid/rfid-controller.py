077df743049a8c6fdcbfbf425cc1299b0d0e7615
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2022-09-03 02:48:30 +0300

    Simulation of RFID-sensors added
    ... to (fake) rfid-controller.py

--- rfid/rfid-controller.py
+++ rfid/rfid-controller.py
@@ -1,10 +1,43 @@
 # /usr/bin/python
-
+# This is a fake controller
 from time import sleep
+import requests as req
+
+def fakeUserChoices():
+  req.get('http://localhost:3000/publish/choice-msg/A-x-x')
+  sleep(4)
+  req.get('http://localhost:3000/publish/choice-msg/B-x-x')
+  sleep(6)
+  req.get('http://localhost:3000/publish/choice-msg/B-D-x')
+  sleep(6)
+  req.get('http://localhost:3000/publish/choice-msg/B-D-E')
+  sleep(15)
+  req.get('http://localhost:3000/publish/choice-msg/A-x-x')
+  sleep(4)
+  req.get('http://localhost:3000/publish/choice-msg/A-F-x')
+  sleep(6)
+  req.get('http://localhost:3000/publish/choice-msg/A-F-C')
+  sleep(15)
+  req.get('http://localhost:3000/publish/choice-msg/A-x-x')
+  sleep(4)
+  req.get('http://localhost:3000/publish/choice-msg/A-F-x')
+  sleep(6)
+  req.get('http://localhost:3000/publish/choice-msg/A-F-E')
+  sleep(15)
+  req.get('http://localhost:3000/publish/choice-msg/B-x-x')
+  sleep(6)
+  req.get('http://localhost:3000/publish/choice-msg/B-D-x')
+  sleep(6)
+  req.get('http://localhost:3000/publish/choice-msg/B-D-E')
 
 if __name__ == "__main__":
   print("fake controller is running...")
   print("Ctr+c to cancel")
 
+  # Let's simulate readings of RFID-sensors
+  sleep(10)
+  fakeUserChoices()
+
+  # then do nothing until stopped
   while True:
     sleep(1)
